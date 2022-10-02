aa9afb27414f5b56b22ebbebfbdf015082f1f76c
CNNunez <nnunez@ic-itcr.ac.cr>
2022-10-01 21:37:13 -0600

    Updating branch

--- ide-triangle-v1.1.src/src/Triangle/XML/WriterVisitor.java
+++ ide-triangle-v1.1.src/src/Triangle/XML/WriterVisitor.java
@@ -656,4 +656,9 @@ public class WriterVisitor implements Visitor {
         return null;
     }
 
+    @Override
+    public Object visitLocalDeclaration(LocalDeclaration ast, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
