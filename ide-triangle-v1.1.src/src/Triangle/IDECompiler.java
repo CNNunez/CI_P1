014800d31041e9cf2ead25f90b65e7830a294989
CNNunez <nnunez@ic-itcr.ac.cr>
2022-10-22 13:38:28 -0600

    Activate Checker

--- ide-triangle-v1.1.src/src/Triangle/IDECompiler.java
+++ ide-triangle-v1.1.src/src/Triangle/IDECompiler.java
@@ -53,9 +53,10 @@ public class IDECompiler {
         
         rootAST = parser.parseProgram();
         if (report.numErrors == 0) {
-//            System.out.println("Contextual Analysis ...");
-//            Checker checker = new Checker(report);
-//            checker.check(rootAST);
+            // Changed by Carolina Narvaez
+            System.out.println("Contextual Analysis ...");
+            Checker checker = new Checker(report);
+            checker.check(rootAST);
             if (report.numErrors == 0) {
 //                System.out.println("Code Generation ...");
 //                Encoder encoder = new Encoder(report);
